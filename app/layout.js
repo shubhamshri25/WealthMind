9a9c317db31c4b4ef16c34c52d3de969d14fff53
shubham <shubhshri25gmail.com>
2025-04-28 14:05:47 +0530

    landing page completed

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
 
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-600">
-              <p>Made with 💗 Shubham</p>
+              <p>Get Started Today 🤑 </p>
             </div>
           </footer>
         </body>
